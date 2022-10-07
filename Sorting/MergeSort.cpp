0ed5519f7b5830361ff1dd6448048d691ceaa88c
Dipti Agarwal <84309022+Dipti821@users.noreply.github.com>
2022-10-07 15:04:11 +0530

    Added merge sort in C++

---
+++ Sorting/MergeSort.cpp
@@ -0,0 +1,58 @@
+// C++ program for Merge Sort
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void merge(vector<int>array, int left, int mid, int right)
+{
+    auto subArrayOne = mid - left + 1;
+    auto subArrayTwo = right - mid;
+
+    vector<int> leftArray(subArrayOne) ,rightArray(subArrayTwo);
+
+    for (auto i = 0; i < subArrayOne; i++)
+        leftArray[i] = array[left + i];
+    for (auto j = 0; j < subArrayTwo; j++)
+        rightArray[j] = array[mid + 1 + j];
+
+    auto indexOfSubArrayOne = 0,   
+        indexOfSubArrayTwo = 0;    
+    int indexOfMergedArray = left;
+
+    while (indexOfSubArrayOne < subArrayOne && indexOfSubArrayTwo < subArrayTwo)
+    {
+        if (leftArray[indexOfSubArrayOne] <= rightArray[indexOfSubArrayTwo])
+        {
+            array[indexOfMergedArray] = leftArray[indexOfSubArrayOne];
+            indexOfSubArrayOne++;
+        }
+        else
+        {
+            array[indexOfMergedArray] = rightArray[indexOfSubArrayTwo];
+            indexOfSubArrayTwo++;
+        }
+        indexOfMergedArray++;
+    }
+    while (indexOfSubArrayOne < subArrayOne)
+    {
+        array[indexOfMergedArray] = leftArray[indexOfSubArrayOne];
+        indexOfSubArrayOne++;
+        indexOfMergedArray++;
+    }
+    while (indexOfSubArrayTwo < subArrayTwo)
+    {
+        array[indexOfMergedArray] = rightArray[indexOfSubArrayTwo];
+        indexOfSubArrayTwo++;
+        indexOfMergedArray++;
+    }
+}
+void mergeSort(vector<int>array, int const begin, int const end)
+{
+    if (begin >= end)
+        return;
+
+    auto mid = begin + (end - begin) / 2;
+    mergeSort(array, begin, mid);   // sort first half
+    mergeSort(array, mid + 1, end); // sort second half
+    merge(array, begin, mid, end);  // merge both half
+}
